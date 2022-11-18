b53a53e118847dd613be85ecc05d1ce5ed53d77f
sekaprin <semenkaprin@yandex.ru>
2022-11-18 18:51:57 +0300

    draw circle bug fixes

--- main.py
+++ main.py
@@ -3,36 +3,37 @@ import sys
 from random import randrange
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtGui import QPainter, QColor, QPolygon
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
-class MyWidget(QWidget):
+
+class MyWidget(QMainWindow):
     def __init__(self):
-        uic.loadUi("UI.ui", self)
         super().__init__()
+        uic.loadUi("UI.ui", self)
         self.initUI()
 
     def initUI(self):
+        self.do_paint = False
         self.setGeometry(600, 200, 800, 800)
-        self.pushButton.clicked.connect(self.paintEvent)
+        self.pushButton.clicked.connect(self.paint)
 
-    def draww(self):
-        self.flag = True
-        self.update()
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
 
     def paintEvent(self, event):
-        if self.flag:
+        if self.do_paint:
             qp = QPainter()
             qp.begin(self)
             self.drawCircle(qp)
             qp.end()
+
+
     def drawCircle(self, qp):
-        qp.setBrush(QColor(255, 55, 0))
-        if self.stats == 1:
-            qp.drawEllipse(randrange(1, 255), randrange(1, 255), randrange(1, 255), randrange(1, 255))
-
-
+        qp.setBrush(QColor(255, 255, 0))
+        size = randrange(1, 255)
+        qp.drawEllipse(randrange(1, 600), randrange(1, 600), size, size)
 
 
 if __name__ == "__main__":
